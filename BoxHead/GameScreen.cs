8ba373eddc561a63e2defe06b8c9f61f90819441
Blaiddcrow <blaiddcrow@gmail.com>
2018-05-24 16:51:58 +0200

    Modified moveCharacter methods to make scroll properly.

--- BoxHead/GameScreen.cs
+++ BoxHead/GameScreen.cs
@@ -10,13 +10,13 @@ class GameScreen : Screen
     protected EnemyGenerator enemyGenerator;
     protected Enemy enemy; // Test enemy.
     protected Level level;
-    protected Character character;
+    public Character Character { get; set; }
     protected Font font;
-    protected short xMap;
-    protected short yMap;
     private int mouseX, mouseY;
     private int mouseClickX, mouseClickY;
     public int Round { get; set; }
+
+    public static int Points;
 
     public GameScreen(Hardware hardware) : base(hardware)
     {
@@ -24,12 +24,11 @@ class GameScreen : Screen
         level = new Level();
         level.ActualLevel = "levels/level1.txt";
         level.Load();
-        character = new Character(300);
-        xMap = 0;
-        yMap = 0;
+        Character = new Character(300);
         Round = 1;
         enemyGenerator = new EnemyGenerator(hardware);
         enemy = new Enemy(100, 25, 1); // Test enemy.
+        Points = Character.Points;
     }
 
     public GameScreen(Hardware hardware, int round) : this(hardware)
@@ -44,92 +43,102 @@ class GameScreen : Screen
         short moveDown = 2;
         short moveLeft = 3;
 
-        bool left = hardware.IsKeyPressed(Hardware.KEY_LEFT);
-        bool right = hardware.IsKeyPressed(Hardware.KEY_RIGHT);
-        bool up = hardware.IsKeyPressed(Hardware.KEY_UP);
-        bool down = hardware.IsKeyPressed(Hardware.KEY_DOWN);
+        bool left = 
+            hardware.IsKeyPressed(Hardware.KEY_LEFT) ||
+            hardware.IsKeyPressed(Hardware.KEY_A);
+        bool right = 
+            hardware.IsKeyPressed(Hardware.KEY_RIGHT) ||
+            hardware.IsKeyPressed(Hardware.KEY_D);
+        bool up = 
+            hardware.IsKeyPressed(Hardware.KEY_UP) ||
+            hardware.IsKeyPressed(Hardware.KEY_W);
+        bool down = 
+            hardware.IsKeyPressed(Hardware.KEY_DOWN) ||
+            hardware.IsKeyPressed(Hardware.KEY_S);
 
         if (up)
         {
-            level.MoveObstacles(moveUp, character.Speed);
-            yMap--;
+            level.MoveObstacles(moveUp, Character.Speed);
+            level.YMap--;
         }
         if (right)
         {
-            xMap++;
-            level.MoveObstacles(moveRight, character.Speed);
+            level.XMap++;
+            level.MoveObstacles(moveRight, Character.Speed);
         }
         if (down)
         {
-            yMap++;
-            level.MoveObstacles(moveDown, character.Speed);
+            level.YMap++;
+            level.MoveObstacles(moveDown, Character.Speed);
         }
         if (left)
         {
-            xMap--;
-            level.MoveObstacles(moveLeft, character.Speed);
+            level.XMap--;
+            level.MoveObstacles(moveLeft, Character.Speed);
         }
     }
 
     public override void Show()
     {
         short centeredCharacterX =
-            (short)((GameController.SCREEN_WIDTH / 2) - (character.Width / 2));
+            (short)((GameController.SCREEN_WIDTH / 2) - (Character.Width / 2));
         short centeredCharacterY =
-           (short)((GameController.SCREEN_HEIGHT / 2) - (character.Height / 2));
+           (short)((GameController.SCREEN_HEIGHT / 2) - (Character.Height / 2));
 
-        character.Image.MoveTo(centeredCharacterX, centeredCharacterY);
-        character.MoveTo(centeredCharacterX, centeredCharacterY);
+        Character.Image.MoveTo(centeredCharacterX, centeredCharacterY);
+        Character.MoveTo(centeredCharacterX, centeredCharacterY);
 
         enemyGenerator.StartRound(1);
 
-        enemy.MoveTo(400, 400);
+        enemy.MoveTo(0, GameController.SCREEN_HEIGHT);
 
         short oldX, oldY, oldXMap, oldYMap;
 
         do
         {
-            if (character.Life > 0)
+            // 1. Draw everything.
+            hardware.UpdateScreen();
+
+            hardware.DrawSprite(level.Floor, 0, 0, level.XMap,
+                level.YMap, GameController.SCREEN_WIDTH,
+                GameController.SCREEN_HEIGHT);
+
+            level.DrawObstacles(hardware);
+            hardware.DrawImage(Character.Image);
+            hardware.DrawImage(enemy.EnemyImage);
+
+            foreach (Enemy enemy in enemyGenerator.enemies)
             {
-                // 1. Draw everything.
-                hardware.UpdateScreen();
-
-                hardware.DrawSprite(level.Floor, 0, 0, level.XMap,
-                    level.YMap, GameController.SCREEN_WIDTH,
-                    GameController.SCREEN_HEIGHT);
-
-                level.DrawObstacles(hardware);
-                hardware.DrawImage(character.Image);
-                hardware.DrawImage(enemy.enemy);
-
-                foreach (Enemy enemy in enemyGenerator.enemies)
-                {
-                    hardware.DrawImage(enemy.enemy);
-                }
-
-                hardware.UpdateScreen();
-                // 2. Move character from keyboard input.
-                oldX = character.X;
-                oldY = character.Y;
-                oldXMap = level.XMap;
-                oldYMap = level.YMap;
-                hardware.GetEvents(
-                    out mouseX, out mouseY, out mouseClickX, out mouseClickY);
-                moveCharacter();
-                character.Animate(mouseX, mouseY);
-                // 3. Move enemies and objects.
-                foreach (Enemy enemy in enemyGenerator.enemies)
-                {
-                    enemy.GoToPlayer(character);
-                }
-
-                enemy.GoToPlayer(character);
-
-                // 4. Check collisions and update game state.
-                if (enemy.CharacterIsOnRange(character))
-                    enemy.Attack(character);
+                hardware.DrawImage(enemy.EnemyImage);
             }
+
+            hardware.UpdateScreen();
+            // 2. Move character from keyboard input.
+            oldX = Character.X;
+            oldY = Character.Y;
+            oldXMap = level.XMap;
+            oldYMap = level.YMap;
+            hardware.GetEvents(
+                out mouseX, out mouseY, out mouseClickX, out mouseClickY);
+            moveCharacter();
+            Character.Animate(mouseX, mouseY);
+            enemy.PonitToCharacter(Character.X, Character.Y);
+
+            // 3. Move enemies and objects.
+            foreach (Enemy enemy in enemyGenerator.enemies)
+                enemy.GoToPlayer(Character);
+
+            enemy.GoToPlayer(Character);
+
+            // 4. Check collisions and update game state.
+            if (enemy.CharacterIsOnRange(Character))
+                enemy.Attack(Character);
         }
-        while (character.Life > 0 && !hardware.IsKeyPressed(Hardware.KEY_ESC));
+        while (Character.Life > 0 && !hardware.IsKeyPressed(Hardware.KEY_ESC));
+    }
+
+    public Character GetCharacter()
+    {
+        return Character;
     }
 }

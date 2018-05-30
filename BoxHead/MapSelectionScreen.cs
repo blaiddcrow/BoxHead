b3b96a0a74493cf5c3e32763b81d40bb7515a1fd
Blaiddcrow <blaiddcrow@gmail.com>
2018-05-30 18:04:45 +0200

    Added level selection screen.

---
+++ BoxHead/MapSelectionScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class MapSelectionScreen : Screen
+{
+    private Image background;
+    private List<IntPtr> levelsTexts;
+    private List<string> levelPaths;
+    private string selectedLevel;
+
+    public MapSelectionScreen(Hardware hardware) : base(hardware)
+    {
+        background = new Image("imgs/others/menuBackground.png", 1280, 720);
+        levelsTexts = new List<IntPtr>();
+        levelPaths = new List<string>();
+        initialiceLevels();
+        selectedLevel = "levels/level1.txt";
+    }
+
+    private void initialiceLevels()
+    {
+        if (Directory.Exists("./levels"))
+        {
+            string[] fileList = Directory.GetFiles("./levels");
+            foreach (string file in fileList)
+            {
+                if (file.StartsWith("level"))
+                {
+                    levelPaths.Add("levels/" + file);
+                }
+            }
+        }
+    }
+
+    public override void Show()
+    {
+        base.Show();
+    }
+}

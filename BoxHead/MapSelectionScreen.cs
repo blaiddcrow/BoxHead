a42e2b3233a764e6b81969429d505d87a44b9fc7
Blaiddcrow <blaiddcrow@gmail.com>
2018-05-30 20:39:13 +0200

    Modified to display all the levels created by the user.

--- BoxHead/MapSelectionScreen.cs
+++ BoxHead/MapSelectionScreen.cs
@@ -1,40 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Tao.Sdl;
 
 class MapSelectionScreen : Screen
 {
     private Image background;
-    private List<IntPtr> levelsTexts;
+    private List<IntPtr> levelsTextRed;
+    private List<IntPtr> levelsTextWhite;
+    private IntPtr pressEnter;
     private List<string> levelPaths;
     private string selectedLevel;
+    private Font font;
+    private int amountOfTexts;
+
+    public int ActualLevel { get; set; }
 
     public MapSelectionScreen(Hardware hardware) : base(hardware)
     {
+        font = new Font("fonts/PermanentMarker-Regular.ttf", 20);
         background = new Image("imgs/others/menuBackground.png", 1280, 720);
-        levelsTexts = new List<IntPtr>();
+        levelsTextRed = new List<IntPtr>();
+        levelsTextWhite = new List<IntPtr>();
         levelPaths = new List<string>();
-        initialiceLevels();
+        pressEnter = new IntPtr();
+        initialiceLevelTexts();
+        amountOfTexts = levelsTextRed.Count;
+        ActualLevel = 0;
         selectedLevel = "levels/level1.txt";
     }
 
-    private void initialiceLevels()
+    private void initialiceLevelTexts()
     {
         if (Directory.Exists("./levels"))
         {
             string[] fileList = Directory.GetFiles("./levels");
             foreach (string file in fileList)
             {
-                if (file.StartsWith("level"))
+                if (file.StartsWith("./levels\\level"))
                 {
-                    levelPaths.Add("levels/" + file);
+                    levelPaths.Add(file);
+
+                    int amount = file.LastIndexOf('.') - 1 - file.LastIndexOf('\\');
+
+                    levelsTextRed.Add(SdlTtf.TTF_RenderText_Solid(
+                    font.GetFontType(), file.Substring(
+                        file.LastIndexOf('\\')+1, amount), hardware.Red));
+
+                    levelsTextWhite.Add(SdlTtf.TTF_RenderText_Solid(
+                    font.GetFontType(), file.Substring(
+                        file.LastIndexOf('\\')+1, amount), hardware.White));
                 }
             }
         }
+
+        pressEnter = SdlTtf.TTF_RenderText_Solid(
+        new Font("fonts/PermanentMarker-Regular.ttf", 20).GetFontType(),
+        "PRESS ENTER TO SELECT A LEVEL", hardware.White);
+    }
+
+    private bool CheckInput()
+    {
+        int key = hardware.KeyPressed();
+
+        bool up = false, down = false;
+        bool optionSelected = (key == Hardware.KEY_ENTER);
+
+        if (key == Hardware.KEY_UP || key == Hardware.KEY_W)
+            up = true;
+        else if (key == Hardware.KEY_DOWN || key == Hardware.KEY_S)
+            down = true;
+
+        if (up)
+            if (ActualLevel == 0)
+                ActualLevel = levelsTextRed.Count - 1;
+            else
+                ActualLevel--;
+        if (down)
+            if (ActualLevel == levelsTextRed.Count - 1)
+                ActualLevel = 0;
+            else
+                ActualLevel++;
+
+        return optionSelected;
+    }
+
+    public Level GetLevel()
+    {
+        Level newLevel = new Level();
+        newLevel.ActualLevel = selectedLevel;
+        return newLevel;
     }
 
     public override void Show()
     {
-        base.Show();
+        bool isOptionSelected = false;
+        background.MoveTo(0, 0);
+
+        do
+        {
+            hardware.ClearScreen();
+            hardware.DrawImage(background);
+
+            for (int level = 0; level < amountOfTexts; level++)
+            {
+                if (level == ActualLevel)
+                    hardware.WriteText(
+                        levelsTextWhite[level], 40, (short)(50 * level));
+                else
+                    hardware.WriteText(
+                        levelsTextRed[level], 40, (short)(50 * level));
+            }
+
+            hardware.WriteText(
+                pressEnter, (short)(GameController.SCREEN_WIDTH / 2 - 150),
+                (short)(GameController.SCREEN_HEIGHT - 30));
+            hardware.UpdateScreen();
+            isOptionSelected = CheckInput();
+
+            if (isOptionSelected)
+                selectedLevel = levelPaths[ActualLevel];       
+        }
+        while (!isOptionSelected);
     }
 }

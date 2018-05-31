ddde2192ff486c3fb48a642c736761d681b45880
Blaiddcrow <blaiddcrow@gmail.com>
2018-05-31 16:42:53 +0200

    Created methods to change between languages.

--- BoxHead/GameController.cs
+++ BoxHead/GameController.cs
@@ -3,19 +3,27 @@
  * which controlls the flow of the program.
  */
 
+using System.IO;
+
 class GameController
 {
     public const short SCREEN_WIDTH = 1280;
     public const short SCREEN_HEIGHT = 720;
+    public bool isInSpanish { get; set; }
+
+    public GameController()
+    {
+        isInSpanish = getLanguage();
+    }
 
     public void Start()
     {
         Hardware hardware = 
             new Hardware(SCREEN_WIDTH, SCREEN_HEIGHT, 24, false);
 
-        WelcomeScreen welcome = new WelcomeScreen(hardware);
-        MenuScreen menu = new MenuScreen(hardware);
-        CreditsScreen credits = new CreditsScreen(hardware);
+        WelcomeScreen welcome = new WelcomeScreen(hardware, this);
+        MenuScreen menu = new MenuScreen(hardware, this);
+        CreditsScreen credits = new CreditsScreen(hardware, this);
 
         hardware.ClearScreen();
         welcome.Show();
@@ -28,5 +36,36 @@ class GameController
             credits.Show();
         }
         while (!menu.GetExit());
+        SetLanguaje();
+    }
+
+    private bool getLanguage()
+    {
+        if (!File.Exists("language.dat"))
+            return false;
+        using (StreamReader input = new StreamReader("language.dat"))
+        {
+            string line = input.ReadLine().ToLower();
+            if (line == "spanish")
+                return true;
+            else
+                return false;
+        }
+    }
+    
+    public void SwitchLanguage()
+    {
+        isInSpanish = !isInSpanish;
+    }
+
+    public void SetLanguaje()
+    {
+        using (StreamWriter output = new StreamWriter("language.dat"))
+        {
+            if (isInSpanish)
+                output.WriteLine("spanish");
+            else
+                output.WriteLine("english");
+        }   
     }
 }
